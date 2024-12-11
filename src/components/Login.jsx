7770996c227cb06d1684519033788cba04cabd73
Admin <shruthida2000@gmail.com>
2024-12-11 23:25:15 +0530

    added input validation after clicking on sign in button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from "./Header"
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMsg, setErrorMsg] = useState('');
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullName = useRef(null);
+
+  const onButtonClickHandler = () => {
+    const message = checkValidData(email.current.value, password.current.value)
+    setErrorMsg(message);
+  }
   return (
     <div>
       <Header />
@@ -12,32 +22,37 @@ const Login = () => {
         alt='background-img'
       />
 
-      <form className="w-3/12 absolute p-12 bg-black bg-opacity-80 my-44 mx-auto right-0 left-0 text-white">
+      <form onSubmit={(e) => e.preventDefault()} className="w-3/12 absolute p-12 bg-black bg-opacity-80 my-44 mx-auto right-0 left-0 text-white">
         <h1 className="font-bold text-3xl py-4 px-1">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h1>
 
         {!isSignIn && <input
+          ref={fullName}
           type='text'
           placeholder="Full Name"
           className="p-4 my-4 w-full bg-gray-700 rounded-md"
         />}
 
         <input
+          ref={email}
           type='text'
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700 rounded-md"
         />
 
         <input
+          ref={password}
           type='password'
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700 rounded-md"
         />
 
+          <p className='text-red-500 font-bold'>{errorMsg}</p>
         <button
           className="p-4 my-4 bg-red-700 w-full rounded-lg">
           {isSignIn ? "Sign In" : "Sign Up"}
+          onClick={onButtonClickHandler}
         </button>
 
         <p
