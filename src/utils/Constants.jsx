f427c786c09ac993b150a067e7a91b4469d2e57a
Shruthidoddmani <shruthida2000@gmail.com>
2025-03-28 20:05:18 +0530

    created constans url seperate

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,1 +1,5 @@
 export const LOGIN_IMG_URL = 'https://assets.nflxext.com/ffe/siteui/vlv3/ce449112-3294-449a-b8d3-c4e1fdd7cff5/web/IN-en-20241202-TRIFECTA-perspective_0acfb303-6291-4ad1-806f-dda785f6295a_medium.jpg'
+
+export const USER_PROFILE_URL = 'https://occ-0-4994-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229'
+
+export const NETFLIX_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
